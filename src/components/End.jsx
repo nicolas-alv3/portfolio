92336b7c8e30f4d812e1df69de6f310a757830ff
Nicolas Alvarez <nicolas.alv3@gmail.com>
2021-02-17 01:12:00 -0300

    Cambia titulo y logo

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Icon, Input } from 'semantic-ui-react';
+import { Icon } from 'semantic-ui-react';
 import "../style/Home.css";
 
 export default function End(props) {
@@ -13,7 +13,7 @@ export default function End(props) {
                         <div><Icon name="mail" size="big" color="yellow" /> nicolas.alv3@gmail.com</div>
                         <div><Icon name="phone" size="big" color="blue" /> 1168896776</div>
                         <div><Icon name="linkedin" size="big" color="blue" /> Nicolas Alvarez</div>
-                    </div>                    
+                    </div>
             </div>
         </div>
     );
